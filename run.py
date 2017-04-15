83243128096670d71667afabe5d041c512211ac0
withoutairs <github@ktcb.com>
2017-04-15 07:04:17 -0500

    starting pointˆ

---
+++ run.py
@@ -0,0 +1,13 @@
+import mintapi
+import keyring
+import datetime
+
+username = keyring.get_keyring().get_password("budget", "username")
+password = keyring.get_keyring().get_password("budget", "password")
+ius_session = keyring.get_keyring().get_password("budget", "ius_session")
+thx_guid = keyring.get_keyring().get_password("budget", "thx_guid")
+mint = mintapi.Mint(username, password, ius_session, thx_guid)
+t = mint.get_transactions()
+filename = "mint-transactions-{}.csv".format(datetime.datetime.now().isoformat()[:10])
+t.to_csv(filename)
+
