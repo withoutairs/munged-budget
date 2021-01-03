a9b78c028a6ccf98e954749baec09a9e45560508
Chris Brown <heychrisbrown@gmail.com>
2021-01-03 09:14:06 -0500

    move to MFA

--- pull-mint.py
+++ pull-mint.py
@@ -1,12 +1,11 @@
 import mintapi
 import keyring
 import datetime
+import os
 
 username = keyring.get_keyring().get_password("budget-username", "username")
 password = keyring.get_keyring().get_password("budget-password", "password")
-ius_session = keyring.get_keyring().get_password("budget-ius_session", "ius_session")
-thx_guid = keyring.get_keyring().get_password("budget-thx_guid", "thx_guid")
-mint = mintapi.Mint(username, password, ius_session, thx_guid)
+mint = mintapi.Mint(username, password, headless=True, mfa_method='soft-token', mfa_token=os.environ['MFA_TOKEN'])
 t = mint.get_transactions()
 filename = "mint-transactions-{}.csv".format(datetime.datetime.now().isoformat()[:10])
 t.to_csv(filename)
